a22771dbac08fe3284d23b68620849688e4638fa
Brendon Murthum <bmmurthum@users.noreply.github.com>
2017-11-11 18:46:05 -0500

    Update ClientHandler.java

--- NapServerGVSU/src/ClientHandler.java
+++ NapServerGVSU/src/ClientHandler.java
@@ -22,9 +22,8 @@ public class ClientHandler extends Thread{
     private String userInformation;
     private String UserName;
     private String UserHostName;
-    private  String UserSpeed;
-    private  String tmpFileName, tmpKeyWords;
-    
+    private String UserSpeed;
+    private String tmpFileName, tmpKeyWords;
     
     /** This contains objects of all the hosted files */
     /* THIS SHOULD BE GLOBAL TO THE ENTIRE SERVER */
@@ -48,11 +47,14 @@ public class ClientHandler extends Thread{
            /* For error handling */
            /* Get IP from the control socket for future connections */
            remoteIP = controlListen.getInetAddress().getHostAddress();
-           System.out.println("A new thread was successfully setup.");
-           System.out.println("");
+           
+           /* For debugging */
+           // System.out.println("  DEBUG: A new thread was successfully setup.");
+           // System.out.println("");
+           
        } catch(IOException ioEx) {
            ioEx.printStackTrace();
-           System.out.println("ERROR: Could not set up a " +
+           System.out.println("  ERROR: Could not set up a " +
                        "threaded client input and output stream.");
        }
    }
@@ -66,257 +68,187 @@ public class ClientHandler extends Thread{
    * 
    ******************************************************************/
     public void run(){   
-    	
-    	
-    	/**
-    	 * TESTING
-    	 */
-    	
-    	//String test1 = CentralServer.descriptions.remove("127.0.0.1");
-    	//System.out.println(test1);
-    	
+      	
     	/* Keeps tracks of when to close the thread */
         boolean stayAlive = true;
         /** Gets user information from the host      */
         StringTokenizer userTokens;
-        try {
-        	/* DEBUG: This shows precisely what is sent over the control-line */ 
-        	userInformation = inFromClient.nextLine();
-        	// System.out.println(userInformation);
-        	         	 
-        	 userTokens = new StringTokenizer(userInformation);
-        	
-        	 UserName = userTokens.nextToken();
-        	 UserName = UserName.replaceAll("@@", " ");
-        	 System.out.println("UserName " + UserName);
-        	 /**
-        	  * Todo: Call Object and find the usernames
-        	  * 
-        	  * 
-        	  */
-        	 //boolean tmpOnServer;
-        	 //tmpOnServer =  Object.GetNameOnServer(UserName);
-        	 //if (tmpOnServer)
-        	 //end connection with client
-        	  
-        	 UserHostName = userTokens.nextToken();
-        	 UserHostName = UserHostName.replaceAll("@@", " ");
-        	 System.out.println("UserHostName " + UserHostName);
-        	 
-        	  
-        	 UserSpeed = userTokens.nextToken();
-        	 System.out.println("UserSpeed " + UserSpeed); 
-        	 
-        	 /* Just for show */
-        	 System.out.println(" ");
-        	 
-        	 /* Variables used in parsing the crazy string */
-        	 String tmp_a_Token, tmpFilename, tmp_c_Token;
-        	 /* First "Filename" */
-        	 tmp_a_Token = userTokens.nextToken();
-        	 /* First "Image.jpg" */
-        	 tmpFileName = userTokens.nextToken();
-        	 String tmpKeywordList = "";
-        	 while(true) {
-        		 /* If there are still tokens left */
-        		 try {
-        			 /* Should mostly be keys */
-        			 tmp_c_Token = userTokens.nextToken();
-        		 } catch (Exception e) {
-        			 /* The last submit, as if "Filename" found again */
-        			 
-        			 /* ADD: addValues () */
-        			 
-        			 System.out.println("Submit: " + tmpFileName + tmpKeywordList);
-        			 break;
-        		 }
-        		 if(tmp_c_Token.equals("FileName")) {
-        			 
-        			 /* ADD: addValues () */
-        			 
-        			 System.out.println("Submit: " + tmpFileName + tmpKeywordList);
-        			 tmpKeywordList = "";
-        			 /* Image.jpg */
-        			 tmpFileName = userTokens.nextToken();
-        		 } 
-        		 else {
-        			 tmpKeywordList = tmpKeywordList + " " + tmp_c_Token; 
-        		 }
-        	 }
-        	 
-        	 /**
-        	  * Louis add bananas
-        	  */
-        	 
-        	 /*
-        	 String tmpToken  ;
-        	 tmpKeyWords = " ";
-        	 boolean newFileDescription = true;
-             while(true)
-             {
-             	try{
-             		tmpToken = userTokens.nextToken();
-             		
-             	}catch (Exception e){
-             		break;
-             	}
-             	
-             	if (tmpToken.equals("FileName")){
-             		
-             		
-             		tmpToken = userTokens.nextToken();
-             		
-             		tmpFileName = tmpToken;
-             				
-             		System.out.println("XFileName: " + tmpToken);
-             	}
-             	
-             	System.out.println("FileName: " + tmpToken);
-             	
-             	while(!(tmpToken.equals("FileName"))){
-             		
-             		tmpToken = userTokens.nextToken();
-             		
-             		if (tmpToken.equals("FileName")) {
-             			tmpToken = userTokens.nextToken();
-             			System.out.println("XXXFileNameXXX: " + tmpToken);
-             			break;
-             		}
-             		
-             		tmpKeyWords = tmpKeyWords + " " + tmpToken;
-             		//System.out.println("Keys: " + tmpToken);
-             		
-             	}
-             	System.out.println("addValues: " + tmpKeyWords);
-             	tmpKeyWords = " ";
-             	
-             	//addValues (String addThisToSpeeds, String addThisToHostname, String addThisToFilename, String addThisToKeywords)
-             	//System.out.println("FileName boolean " + tmpFileName );
-             	//newFileDescription = true;
-             	
-             	
-             	
-             	
-             	
-             	
-             	
-             
-             }	 
-             //System.out.println("Keys: " + tmpKeyWords);
-              */
-        	 
-        	 
-        	 
-        	 
-        }
-             
         
-        catch (Exception e) {
+        try {
+            /* This grabs the string of data with filenames and keys */
+        	userInformation = inFromClient.nextLine();
+        	
+        	/* For Debugging. This shows what is read from control-line. */ 
+        	// System.out.println("  DEBUG: Read-In: " + userInformation);
+        	
+        	/* Make the string parseable by tokens */         	 
+        	userTokens = new StringTokenizer(userInformation);
+        	
+        	/*
+        	 TODO - Check User's username for copies.
+        	 Brendon - NOV 11, 2017
+        	 We can error handle multiple users on the same IP (two people 
+        	 from the same house) by checking THIS username against the 
+        	 stored ones. On User-SUCCESS, of choosing a unique one, 
+        	 continue as planned. On User-ERROR, of choosing a taken one,
+        	 never show the main table of info to the user, and break the 
+        	 current thread.
+        	 A failure to the user will look as nothing happened, while
+        	 success will look as a table generated.
+        	*/
+        	
+        	/* Initialize and display the new user's shown username */
+            UserName = userTokens.nextToken();
+            UserName = UserName.replaceAll("@@", " ");
+            System.out.println("Username: " + UserName);
+                        
+            /* 
+             TODO - Change UserHostName
+             Brendon - NOV 11, 2017
+             The User's IP is grabbed directly from the stream by our
+             methods. Right now, the variable remoteIP equals the user's 
+             actual IP regardless of what the user typed in. I propose we 
+             change UserHostName to be strictly the User's 
+             typed-computer-name, to have our program paste the ACTUAL IP 
+             to the end. Currently, a User could type in a false IP to 
+             later have another user ping the wrong address.
+            */
+
+            /* Initialize and display the new user's hostname and IP */
+            UserHostName = userTokens.nextToken();
+            UserHostName = UserHostName.replaceAll("@@", " ");
+            System.out.println("UserHostname: " + UserHostName);
+
+            /* Initialize and display the new user's speed */
+            UserSpeed = userTokens.nextToken();
+            System.out.println("UserSpeed: " + UserSpeed); 
+
+            /* Display new user's IP */
+            System.out.println("IP Address: " + remoteIP); 
+
+            /* Just for show */
+            System.out.println(" ");
+        	 
+            /* Variables used in parsing the crazy string */
+            String tmp_a_Token, tmpFilename, tmp_c_Token;
+            /* First "Filename" */
+            tmp_a_Token = userTokens.nextToken();
+            /* First "Image.jpg" */
+            tmpFileName = userTokens.nextToken();
+            String tmpKeywordList = "";
+            while(true) {
+                /* If there are still tokens left */
+                try {
+                    /* Should mostly be keys */
+                    tmp_c_Token = userTokens.nextToken();
+                } catch (Exception e) {
+                    /* The last submit, as if "Filename" found again */
+
+                    /* TODO ADD: addValues () */
+
+                    System.out.println("Submit: " + tmpFileName + tmpKeywordList);
+                    break;
+                }
+                if(tmp_c_Token.equals("FILENAME")) {
+
+                    /* TODO ADD: addValues () */
+
+                    System.out.println("Submit: " + tmpFileName + tmpKeywordList);
+                    tmpKeywordList = "";
+                    /* Image.jpg */
+                    tmpFileName = userTokens.nextToken();
+                } 
+                else {
+                    tmpKeywordList = tmpKeywordList + " " + tmp_c_Token; 
+                }
+            }
+             
+        } catch (Exception e) {
             /* Host did not supply user information  */
             System.out.println("");
-            System.out.println("ERROR: Host did not supply user information");
+            System.out.println("  ERROR: Host did not supply user information");
+            System.out.println("  ERROR: Ending user's thread");
+            
+            /* End the thread */
+            return;
         }
         
-       // ArrayList<String[]> FileNamewKeys = new Arraylist();
-        /**
-         * Todo: Parse filenames, keys out of string 
-         * 
-         * FileName pexels-photo-218863.jpeg             
-         * Image Keyboard Laptop Stock Image Mac jpeg          
-         * FileName pexels-photo-225223.jpeg             
-         * Image Cell Phone Breakfast Soda Stock Image jpeg          
-         * FileName pexels-photo-225767.jpeg             
-         * Image Laptop Keyboard Paper Pen jpeg         
-         * FileName pexels-photo-4545	7.jpeg 
-         * 
-         * 
-         */
-        
-	        
-	        
+        /* TODO - Add this? BRENDON, Nov 11. */
         //descriptions.add(UserSpeed, UserHostName, tmpFileName, tmpKeyWords);
-        
         
         /* The controlling loop that keeps the user alive */
         while (stayAlive) {
         	
-        	 /* This reads the command from the client */
-        	 try {
-                 recvMsg = inFromClient.nextLine();
-                 
-             } catch (Exception e) {
-                 /* Client left early, or otherwise */
-                 System.out.println("");
-                 System.out.println("ERROR: Client left early");
-                 break;
-             }
-             String returnedString = "";
-             String currentToken;
-             StringTokenizer tokens = new StringTokenizer(recvMsg);
-            
-             String clientDataPort = tokens.nextToken();
+            /* This reads the command from the client */
+            try {
+                recvMsg = inFromClient.nextLine();
+                
+            } catch (Exception e) {
+                /* Client left early, or otherwise */
+                System.out.println("Client " + UserName + " [" + remoteIP + "] left early!");
+                break;
+            }
+            String returnedString = "";
+            String currentToken;
+            StringTokenizer tokens = new StringTokenizer(recvMsg);
+
+            String clientDataPort = tokens.nextToken();
+
+            /* Client command, "UPDATE" or another. */
+            String commandFromClient = tokens.nextToken();
              
-             /* Client command, "LIST," or another. */
-             String commandFromClient = tokens.nextToken();
+            String key, fileName, keyWords; 
              
-             String key, fileName, keyWords; 
-             
-             /* While checks for more user input in the token */
-             while (tokens.hasMoreTokens()) {
+            /* While checks for more user input in the token */
+            while (tokens.hasMoreTokens()) {
             	 
-                 /* Client opened dataport, "1079," for the server to
-                    connect to */
-            	 /* NOTE - This "clientDataPort" may be changed later. It was added to
-            	  * have continuity with the FTP-server that we developed. Oct 25.
-            	  */
+                /* Client opened dataport, "1079," for the server to
+                   connect to */
+                /* NOTE - This "clientDataPort" may be changed later. It was added to
+                 * have continuity with the FTP-server that we developed. Oct 25.
+                 */
                  
-                 
-                 /* This command closes this client-thread */
-	             if(commandFromClient.equals("QUIT")){
-	            	 System.out.println("Quitting from Client Handler");
-	             	 
-	            	 stayAlive = false;	
-	            	 break;   
-	             }
-	             /* This commands initiates the keyword-search */
-	             else if(commandFromClient.equals("KEYWORD")){
+                /* This command closes this client-thread */
+                if(commandFromClient.equals("QUIT")){
+                    System.out.println("Client " + UserName + " [" + remoteIP + "] disconnected!");
+                    stayAlive = false;	
+                    break;   
+                }
+                /* This commands initiates the keyword-search */
+                else if(commandFromClient.equals("KEYWORD")){
+                    System.out.println("Client " + UserName + " [" + remoteIP + "]: ");
+                    System.out.println("COMMAND: KEYWORD");
+                    /*Passes the last arguement into the key */
+                    key = tokens.nextToken();
+                    System.out.println("Key " + key);
+                }
+                /* This command updates the files the host have in their root directory */ 
+                else if(commandFromClient.equals("UPDATE")){
+                    System.out.println("Client " + UserName + " [" + remoteIP + "]: ");
+                    System.out.println("COMMAND: UPDATE");
+
+                    /* Passes the last arguement into the key */
+                    fileName = tokens.nextToken();
+                    System.out.println("Filename: " + fileName);
+
+                    keyWords = tokens.nextToken();
+                    System.out.println("keyWords " + keyWords);
+
+                    /* TODO - Is this to current? BRENDON, NOV 11. */
+                    /* Set up one "File" object to be updated on server */
+                    FileContainer tempContainer = new FileContainer();
+                    tempContainer.setFileName(fileName);
+                    tempContainer.setHostIP("TEMP");
+                    tempContainer.setSpeed("TEMP");
+                    tempContainer.setKeyString("TEMP");
+
+                    /* Adds this object to the general list of hosted files */
+                    AllFiles.add(tempContainer);
 	            	 
 	            	 
-	            	 System.out.println("COMMAND: KEYWORD from user");
-	            	 /*Passes the last arguement into the key */
-	            	 key = tokens.nextToken();
-	            	 System.out.println("Key " + key);
-	            	 
-	             }
-	             /* This command updates the files the host have in their root directory */ 
-	             else if(commandFromClient.equals("UPDATE")){
-	            	 
-	            	 
-	            	 System.out.println("COMMAND: Update from user");
-	            	 /*Passes the last arguement into the key */
-	            	 
-	            	 fileName = tokens.nextToken();
-	            	 
-	            	 System.out.println("File Name: " + fileName);
-	            	 
-	            	 keyWords = tokens.nextToken(); 
-	            	 
-	            	 System.out.println("keyWords " + keyWords);
-	            	 
-	            	 /* Set up one "File" object to be updated on server */
-	            	 FileContainer tempContainer = new FileContainer();
-	            	 tempContainer.setFileName(fileName);
-	            	 tempContainer.setHostIP("TEMP");
-	            	 tempContainer.setSpeed("TEMP");
-	            	 tempContainer.setKeyString("TEMP");
-	            	 
-	            	 /* Adds this object to the general list of hosted files */
-	            	 AllFiles.add(tempContainer);
-	            	 
-	            	 
-	             }
+                }
 	             
-	         }
+            }
                 
         /* End of the other while loop */
         }
