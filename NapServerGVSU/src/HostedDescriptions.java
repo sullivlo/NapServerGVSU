2a6a6b880bd29bfdc116613f3d1739a2a34848df
ramijavi <ramijavi@mail.gvsu.edu>
2017-11-10 19:35:03 -0500

    Update HostedDescriptions.java

--- NapServerGVSU/src/HostedDescriptions.java
+++ NapServerGVSU/src/HostedDescriptions.java
@@ -5,18 +5,21 @@ public class HostedDescriptions {
 	private ArrayList<String> speeds;
 	private ArrayList<String> hostnames;
 	private ArrayList<String> filenames;
+	private ArrayList<String> keywords;
 
 	public HostedDescriptions() {
 			
 		speeds = new ArrayList<String>();
 		hostnames = new ArrayList<String>();
 		filenames = new ArrayList<String>();
+		keywords = new ArrayList<String>();
 	}
 	
-	public void addValues (String addThisToSpeeds, String addThisToHostname, String addThisToFilename) {
+	public void addValues (String addThisToSpeeds, String addThisToHostname, String addThisToFilename, String addThisToKeywords) {
 		this.speeds.add(addThisToSpeeds);
 		this.hostnames.add(addThisToHostname);
 		this.filenames.add(addThisToFilename);
+		this.keywords.add(addThisToKeywords);
 	}
 		
 	public void remove (String hostname) {
@@ -25,6 +28,7 @@ public class HostedDescriptions {
 				hostnames.remove(i);
 				speeds.remove(i);
 				filenames.remove(i);
+				keywords.remove(i);
 				i--;
 			}
 		}
